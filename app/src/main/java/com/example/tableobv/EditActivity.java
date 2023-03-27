52c7694a7a7b006f0fcb07fc4493169f04987416
MazutaShashlik <fafkararka@mail.ru>
2023-03-27 13:52:15 +0900

    op

--- app/src/main/java/com/example/tableobv/EditActivity.java
+++ app/src/main/java/com/example/tableobv/EditActivity.java
@@ -1,14 +1,98 @@
 package com.example.tableobv;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 public class EditActivity extends AppCompatActivity {
+
+    private ImageView imItem;
+    private StorageReference mStorageRef;
+    private Uri uploadUri;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_layout);
+        init();
+    }
+    private void init()
+    {
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+        imItem = findViewById(R.id.imageProduct);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 10 && data != null && data.getData() != null)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                imItem.setImageURI(data.getData());
+                uploadImage();
+            }
+        }
+    }
+
+    private void uploadImage()
+    {
+        Bitmap bitMap = ((BitmapDrawable)imItem.getDrawable()).getBitmap();
+        ByteArrayOutputStream out  = new ByteArrayOutputStream();
+        bitMap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+        byte[] byteArray= out.toByteArray();
+        final StorageReference mRef = mStorageRef.child(System.currentTimeMillis() + "_image");
+        UploadTask up =mRef.putBytes(byteArray);
+        Task<Uri> task = up.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return mRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                uploadUri = task.getResult();
+                Toast.makeText(EditActivity.this, "upload done: " + uploadUri.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    public void onClickImage(View view)
+    {
+        getImage();
+    }
+
+    private void  getImage()
+    {
+        Intent intent  = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 10);
     }
 }
